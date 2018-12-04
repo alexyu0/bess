57100886960b4b01f30290d3fe19974b7b95f6cf
Ubuntu <ubuntu@ip-172-31-6-169.ec2.internal>
2018-12-04 04:43:57 +0000

    sends pkts while trying to filter out retransmitted pkts

--- core/modules/tcp_sender.cc
+++ core/modules/tcp_sender.cc
@@ -1,33 +1,3 @@
-// Copyright (c) 2014-2016, The Regents of the University of California.
-// Copyright (c) 2016-2017, Nefeli Networks, Inc.
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//
-// * Redistributions of source code must retain the above copyright notice, this
-// list of conditions and the following disclaimer.
-//
-// * Redistributions in binary form must reproduce the above copyright notice,
-// this list of conditions and the following disclaimer in the documentation
-// and/or other materials provided with the distribution.
-//
-// * Neither the names of the copyright holders nor the names of their
-// contributors may be used to endorse or promote products derived from this
-// software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-
 #include "tcp_sender.h"
 
 #include "../utils/ether.h"
@@ -53,15 +23,10 @@ CommandResponse TCPSender::Init(const bess::pb::EmptyArg &) {
   return CommandSuccess();
 }
 
-/*struct task_result TCPSender::RunTask(Context *, bess::PacketBatch *, void *) {
-  return {.block = false, .packets = 0, .bits = 0};
-}
-*/
 
 void TCPSender::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
   //gate_idx_t igate = ctx->current_igate;
 
-  //LOG(INFO) << "hello from process batch";
 
   int cnt = batch->cnt();
 
@@ -70,37 +35,43 @@ void TCPSender::ProcessBatch(Context *ctx, bess::PacketBatch *batch) {
   for (int i = 0; i < cnt; i++) {
     bess::Packet *pkt = batch->pkts()[i];    
     
-    LOG(INFO) << "Got pkt from batch...";
 
     /* Start copy from url_filter to get TCP header */
     Ethernet *eth = pkt->head_data<Ethernet *>();
     Ipv4 *ip = reinterpret_cast<Ipv4 *>(eth + 1);
 
-    if (ip->protocol != Ipv4::Proto::kTcp) {
+    /*if (ip->protocol != Ipv4::Proto::kTcp) {
       EmitPacket(ctx, pkt, 1); // Drop
       continue;
-    }
+    }*/
 
     int ip_bytes = ip->header_length << 2;
     Tcp *tcp = reinterpret_cast<Tcp *>(reinterpret_cast<uint8_t *>(ip) + ip_bytes);
     /* End copy from url_filter to get TCP header */
 
-    LOG(INFO) << "Processing pkt as TCP...";
 
 
+    LOG(INFO) << "Pkt seq num = " << tcp->seq_num << ", pkt len = " << ip->length << ", last seq = " << last_seen_seq;
 
-    if (tcp->seq_num > last_seen_seq) {
-      last_seen_seq = tcp->seq_num;
-    } else {
-      LOG(INFO) << "Dropping pkt with seq num " << tcp->seq_num;
-      EmitPacket(ctx, pkt, 1); // Drop
-      continue;
+    bess::utils::be16_t pkt_len = ip->length;
+    bess::utils::be32_t next_expected_seq = last_seen_seq + bess::utils::be32_t(pkt_len.raw_value());
+
+    // not keep-alive and (non-zero length or syn or fin) and next expected > curr seq
+    if (!(tcp->flags & Tcp::Flag::kAck && pkt_len > bess::utils::be16_t(0))) {
+      if (pkt_len > bess::utils::be16_t(0) || tcp->flags & Tcp::Flag::kSyn || tcp->flags & Tcp::Flag::kFin) {
+        if (next_expected_seq > tcp->seq_num) {
+          LOG(INFO) << "Dropping pkt with seq num " << tcp->seq_num;
+          EmitPacket(ctx, pkt, 0); // Drop
+          continue;
+        }
+      }
     }
 
+    last_seen_seq = tcp->seq_num;
 
+    LOG(INFO) << "Emitting pkt with seq num " << tcp->seq_num;
     EmitPacket(ctx, pkt, 0);
   }
-
 }
 
 ADD_MODULE(TCPSender, "tcp_sender", "sends pseudo TCP messages")
