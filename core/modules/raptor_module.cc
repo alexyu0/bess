e976affc9f185cf47bbe797d153f67140074c50b
Alex Yu <alexyu0925@gmail.com>
2018-12-04 08:54:30 -0500

    fix includes

--- core/modules/raptor_module.cc
+++ core/modules/raptor_module.cc
@@ -28,6 +28,7 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#include "raptor_module.h"
 #include <vector>
 #include <unistd.h>
 
